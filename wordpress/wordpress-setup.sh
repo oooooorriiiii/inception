a9c8bfba9f7bef6de7a1d335963d7be5aaa615c9
ymori <ymori@student.42tokyo.jp>
2022-11-20 03:34:24 +0900

    update: use wp #4

--- wordpress/wordpress-setup.sh
+++ wordpress/wordpress-setup.sh
@@ -1,7 +1,37 @@
 #!/bin/sh
 
-sed -i "s/define('DB_NAME', 'database_name_here');/define('DB_NAME', '$MYSQL_DATABASE_NAME');/g" /usr/src/wordpress/wp-config-sample.php
-sed -i "s/define('DB_USER', 'username_here');/define('DB_USER', '$MYSQL_USER');/g" /usr/src/wordpress/wp-config-sample.php
-sed -i "s/define('DB_PASSWORD', 'password_here');/define('DB_PASSWORD', '$MYSQL_PASSWORD');/g" /usr/src/wordpress/wp-config-sample.php
-sed -i "s/define('DB_HOST', 'localhost');/define('DB_HOST', '$MYSQL_HOST_NAME');/g" /usr/src/wordpress/wp-config-sample.php
+while ! mariadb -h$MYSQL_HOST -u$WP_DB_USER -p$WP_DB_PASSWORD $WP_DB_NAME --silent; do
+    echo "Waiting for database connection..."
+    sleep 1
+done
 
+wp core download --allow-root
+
+wp config create \
+    --allow-root \
+    --dbname=${WP_DB_NAME} \
+    --dbuser=${WP_DB_USER} \
+    --dbpass=${WP_DB_PASSWORD} \
+    --dbhost=${MYSQL_HOST}
+
+wp core install \
+    --allow-root \
+    --url=${WP_URL} \
+    --title=${WP_TITLE} \
+    --admin_user=${WP_ADMIN_USER} \
+    --admin_password=${WP_ADMIN_PASSWORD} \
+    --admin_email=${WP_ADMIN_EMAIL}
+
+wp user create \
+    --allow-root \
+    --role=author \
+    --user_pass=${WP_AUTHOR_PASSWORD} \
+    ${WP_AUTHOR_USER}
+
+wp theme install \
+    --allow-root \
+    ${WP_THEME}
+
+mkdir -p /var/run/php-fpm
+
+php-fpm --nodemonize
