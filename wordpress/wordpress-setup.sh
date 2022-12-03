61234f711b2ecc34d6b069de694b93f2d1b182a1
ymori <ymori@student.42tokyo.jp>
2022-12-03 19:32:18 +0900

    wordpress

--- wordpress/wordpress-setup.sh
+++ wordpress/wordpress-setup.sh
@@ -12,12 +12,26 @@ if [ ! -f "/var/www/html/$WP_FILE_ONINSTALL" ]; then
 
 	# wp-cli
 	wp core download --allow-root
-	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWORD \
-		--dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USER \
-		--admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
-	wp theme install ryu --activate --allow-root
+	wp config create \
+        --dbname=$WP_DB_NAME \
+        --dbuser=$WP_DB_USER \
+        --dbpass=$WP_DB_PASSWORD \
+		--dbhost=$MYSQL_HOST \
+        --dbcharset="utf8" \
+        --dbcollate="utf8_general_ci" \
+        --allow-root
+	wp core install \
+        --url=$DOMAIN_NAME/wordpress \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN_USER \
+		--admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --skip-email \
+        --allow-root
+	wp user create $WP_USER $WP_EMAIL \
+        --role=author \
+        --user_pass=$WP_PASSWORD \
+        --allow-root
 
 	# redis
     # sed -i "40i define('WP_REDIS_HOST', '$REDIS_HOST');" wp-config.php
